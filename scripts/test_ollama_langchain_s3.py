410a69e9102df74eb764b92b846295b5a9518402
Ben abdallah youssef <benabdallahyoussef2020@gmail.com>
2026-07-17 19:30:19 +0100

    ajoutzer des commentaires

--- scripts/test_ollama_langchain_s3.py
+++ scripts/test_ollama_langchain_s3.py
@@ -1,7 +1,7 @@
 from langchain_ollama import ChatOllama
 
 llm = ChatOllama(model="phi3", temperature=0)
-
+# llm = ChatOllama(model="ollama/phi3", temperature=0)  # alternative
 question = input("Posez votre question : ")
 reponse = llm.invoke(question)
 
